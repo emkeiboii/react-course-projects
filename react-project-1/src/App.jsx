9876dc0b5a6dd6662f79957dd0c3537ce1957a5d
Mark <marklajom.work@gmail.com>
2023-08-23 11:44:45 +0200

    fetch advice api to console

--- react-project-1/src/App.jsx
+++ react-project-1/src/App.jsx
@@ -1,8 +1,14 @@
 export default function App() {
+  async function getAdvice() {
+    const res = await fetch("https://api.adviceslip.com/advice");
+    const data = await res.json();
+    console.log(data.slip.advice);
+  }
+
   return (
     <div>
       <h1>Hello World!</h1>
-      <button>Get advice</button>
+      <button onClick={getAdvice}>Get advice</button>
     </div>
   );
 }
