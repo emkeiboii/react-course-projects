18abf53d3ac2844ae300cab37519dbe73d19778b
Mark <marklajom.work@gmail.com>
2023-08-24 19:11:31 +0200

    De-structuring reviewed

--- JS-Review/script.js
+++ JS-Review/script.js
@@ -142,3 +142,21 @@ function getBooks() {
 function getBook(id) {
   return data.find((d) => d.id === id);
 }
+
+// Destructuring
+
+const book = getBook(1);
+
+// const title = book.title;
+
+// const author = book.author;
+
+const { title, author, genres } = book;
+
+// const primaryGenre = genres[0];
+
+// const secondaryGenre = genres[1]
+
+const { primaryGenre, secondaryGenre } = genres;
+
+console.log(author, title, primaryGenre);
