3ff6e04119c2b630eefb613d6e4b4d12a705d307
Mark <marklajom.work@gmail.com>
2024-01-09 10:41:05 +0100

    get currencies via API

--- currency-converter/src/App.jsx
+++ currency-converter/src/App.jsx
@@ -1,13 +1,14 @@
-//
+// https://api.frankfurter.app/currencies
 
 import { useEffect, useState } from "react";
 
 export default function App() {
   const [value, setValue] = useState(0);
-  const [currency1, setCurrency1] = useState("USD");
-  const [currency2, setCurrency2] = useState("EUR");
+  const [currency1, setCurrency1] = useState("EUR");
+  const [currency2, setCurrency2] = useState("USD");
   const [convertedValue, setConvertedValue] = useState("");
   const [isLoading, setIsLoading] = useState(false);
+  const [currencyList, setCurrencyList] = useState([]);
 
   useEffect(
     function () {
@@ -29,6 +30,20 @@ export default function App() {
     [value, currency1, currency2]
   );
 
+  useEffect(function () {
+    const controller = new AbortController();
+    async function getCurrencies() {
+      const res = await fetch(`https://api.frankfurter.app/currencies`, {
+        signal: controller.signal,
+      });
+
+      const data = await res.json();
+      const currencyKeys = Object.keys(data);
+      setCurrencyList(currencyKeys);
+      console.log(currencyKeys);
+    }
+    getCurrencies();
+  }, []);
   function handleReverse() {
     setCurrency1(currency2);
     setCurrency2(currency1);
@@ -46,10 +61,9 @@ export default function App() {
           value={currency1}
           onChange={(e) => setCurrency1(e.target.value)}
         >
-          <option value="USD">USD</option>
-          <option value="EUR">EUR</option>
-          <option value="CAD">CAD</option>
-          <option value="INR">INR</option>
+          {currencyList.map((currency) => (
+            <option key={currency}>{currency}</option>
+          ))}
         </select>
       </div>
       <button className="btn-reverse" onClick={handleReverse}>
@@ -71,10 +85,9 @@ export default function App() {
           value={currency2}
           onChange={(e) => setCurrency2(e.target.value)}
         >
-          <option value="USD">USD</option>
-          <option value="EUR">EUR</option>
-          <option value="CAD">CAD</option>
-          <option value="INR">INR</option>
+          {currencyList.map((currency) => (
+            <option key={currency}>{currency}</option>
+          ))}
         </select>
       </div>
     </div>
